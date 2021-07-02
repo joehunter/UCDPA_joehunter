af2b17b1b096d8ff2cf9d146cc888c1189107f2c
joehunter <joe.hunter@yahoo.co.uk>
2021-07-02 16:12:18 +0100

    Added input option to choose local or downloaded data

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import os, shutil
 import zipfile
 import glob
 
-from kaggle.api.kaggle_api_extended import KaggleApi
-api = KaggleApi()
-api.authenticate()
-
 
 def use_local_copy_of_house_price_data():
+    """
+    Reads a CSV file stored in local folder called Data\local\ relative to
+    the main code.
+
+    Returns: DataFrame
+    """
     return pd.read_csv(".\Data\Local\Property_Price_Register_Ireland_2021-05-28.csv")
+
 
 def delete_all_files_from_this_folder(folder):
     for filename in os.listdir(folder):
@@ -25,14 +28,15 @@ def delete_all_files_from_this_folder(folder):
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-def download_house_price_data_from_kaggle_api():
-    path_name = './Data/From_api/'
-    delete_all_files_from_this_folder(path_name)
-    api.dataset_download_files('erinkhoo/property-price-register-ireland',path=path_name)
+def download_house_price_data_from_kaggle_api(this_path_name):
+    from kaggle.api.kaggle_api_extended import KaggleApi
+    api = KaggleApi()
+    api.authenticate()
 
-    for name in glob.glob('./Data/From_api/*'):
+    delete_all_files_from_this_folder(this_path_name)
+    api.dataset_download_files('erinkhoo/property-price-register-ireland',path=this_path_name)
+    for name in glob.glob(this_path_name + '*.zip'):
         dataset_zip_name = name
-
     return dataset_zip_name
 
 
@@ -41,5 +45,22 @@ def unzip_this_zip_file(this_path_and_zip_file_name, extract_to):
         zipref.extractall(extract_to)
 
 
-path_and_zip_file_name = download_house_price_data_from_kaggle_api()
-#unzip_this_zip_file(path_and_zip_file_name,'./Data/')
+def use_downloaded_copy_of_house_price_data():
+    path_name = './Data/From_api/'
+    path_and_zip_file_name = download_house_price_data_from_kaggle_api(path_name)
+    unzip_this_zip_file(path_and_zip_file_name, path_name)
+    for name in glob.glob(path_name+'*.csv'):
+        csv_file_name = name
+    return pd.read_csv(csv_file_name)
+
+
+choose_data = input("Press 1 to choose local downloaded data OR press 2 to download from Kaggle(needs an API)? ")
+
+if choose_data == 1:
+    print('Using local data now...please wait')
+    df = use_local_copy_of_house_price_data()
+else:
+    print('Downloading from Kaggle...please wait')
+    df = use_downloaded_copy_of_house_price_data()
+
+
