31a02379323e4c8dcab97998670bb07a4cb4e67a
joehunter <joe.hunter@yahoo.co.uk>
2021-07-15 11:57:56 +0100

    Switch to using Melbourne Housing Data

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def use_local_copy_of_data():
 
     Returns: DataFrame
     """
-    return pd.read_csv(".\Data\Local\Breast_Cancer_Wisconsin_Diagnostic_Data_Set_v2021-07-10.csv")
+    return pd.read_csv(".\Data\Local\Melbourne_housing_v2021-07-15.csv")
 
 
 def delete_all_files_from_this_folder(folder):
@@ -32,11 +32,10 @@ def download_data_from_kaggle_api(this_path_name):
     api = KaggleApi()
     api.authenticate()
 
-    #https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
 
     delete_all_files_from_this_folder(this_path_name)
-    #api.dataset_download_files('erinkhoo/property-price-register-ireland',path=this_path_name)
-    api.dataset_download_files('uciml/breast-cancer-wisconsin-data', path=this_path_name)
+
+    api.dataset_download_files('anthonypino/melbourne-housing-market', path=this_path_name)
 
     for name in glob.glob(this_path_name + '*.zip'):
         dataset_zip_name = name
