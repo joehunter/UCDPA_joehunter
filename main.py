7bf916542e34a50f6718dc1f87f4f58271726ab0
joehunter <joe.hunter@yahoo.co.uk>
2021-07-24 13:34:36 +0100

    Added report output

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import webbrowser
+import os
 
 
 import file_handling
@@ -6,10 +11,7 @@ import discovery
 import regression
 import regularization
 
-
 choose_data = int(input("Press 1 to choose local data already downloaded OR press 2 to download latest data from Kaggle(requires an API Token)?"))
-
-
 
 if choose_data == 1:
     print('Using local data now...please wait')
@@ -23,10 +25,28 @@ print(df.shape)
 print()
 print()
 
+df['log_Price'] = np.log1p(df.Price.dropna())
+
+log_price_mean = df['log_Price'].mean()
+log_price_std = df['log_Price'].std()
+
+# view log(price) data
+df.log_Price.hist(bins=20)
+plt.axvline((log_price_mean+log_price_std), color='k', linestyle='--')
+plt.axvline((log_price_mean-log_price_std), color='k', linestyle='--')
+plt.axvline(log_price_mean, color='k', linestyle='-')
+plt.title('log(Price)')
+plt.show()
+
+print(df.shape)
+
 #cleaning.CleanData(df)
 #discovery.EDA(df)
 #regression.Linear(df)
 #regularization.Lasso(df)
 
+new = 2 # open in a new tab, if possible
+url = "file://c:/UCDPA_joehunter/Output/report.htm"
+#webbrowser.open(url, new=new)
 
-
+webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open(url)
