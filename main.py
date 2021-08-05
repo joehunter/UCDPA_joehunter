4947579556102bacc6c200a31733246e39602fcd
joehunter <joe.hunter@yahoo.co.uk>
2021-08-05 11:23:38 +0100

    Incorporate rows with no price

--- main.py
+++ main.py
@@ -21,10 +21,13 @@ data_test_raw = df.loc[df.Price.isnull(), :]
 
 print('Training samples = {}\nTesting samples = {}\nTrain-test ratio = {}'.format(len(data_train_raw), len(data_test_raw), round(len(data_train_raw)/len(data_test_raw), 1)))
 
+
 discovery.EDA(df)
+
 
 data_cleaning = cleaning.CleanData(df)
 df = data_cleaning.return_df()
+
 
 pre_processing = preprocessing.Encode(df)
 df = pre_processing.return_df()
