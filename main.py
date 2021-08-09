1ba66740d361738a646399f43ab8ecbd1499fdb0
joehunter <joe.hunter@yahoo.co.uk>
2021-08-09 10:07:26 +0100

    Renamed file_handling class to importing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import file_handling
+import importing
 import cleaning
 import discovery
 import preprocessing
@@ -9,22 +9,24 @@ choose_data = int(input("Press 1 to choose local data already downloaded OR pres
 
 if choose_data == 1:
     print('Using local data now...please wait')
-    fh = file_handling.Import(1)
+    fh = importing.Import(1)
 else:
     print('Downloading from Kaggle...please wait')
-    fh = file_handling.Import(0)
+    fh = importing.Import(0)
 
 df = fh.return_df()
 
-discovery.EDA(df)
 
-data_cleaning = cleaning.CleanData(df)
-df = data_cleaning.return_df()
 
-pre_processing = preprocessing.Encode(df)
-df = pre_processing.return_df()
-
-modelling.RunModels(df)
-
-tuning.TuneModel(df)
+# discovery.EDA(df)
+#
+# data_cleaning = cleaning.CleanData(df)
+# df = data_cleaning.return_df()
+#
+# pre_processing = preprocessing.Encode(df)
+# df = pre_processing.return_df()
+#
+# modelling.RunModels(df)
+#
+# tuning.TuneModel(df)
 
