38d271156fc87e2d74e5ef651e5e3aa6bde92ba1
joehunter <joe.hunter@yahoo.co.uk>
2021-08-09 18:42:02 +0100

    Fixed bug passing  raw data to analysis class

--- insights.py
+++ insights.py
@@ -4,27 +4,29 @@ class Visualize:
 
     def __init__(self, this_df):
 
+        this_df = this_df[this_df['Longtitude'].notna()]
+        this_df = this_df[this_df['Lattitude'].notna()]
+        this_df = this_df[this_df['Price_LG'].notna()]
+        this_df['Property_Type'] = this_df.apply(lambda row: self.fetch_type_description(row), axis=1)
+        this_df['Region'] = this_df.apply(lambda row: self.shorten_region_name(row), axis=1)
+
         self.map_prices(this_df)
+        self.scatter_plot_distance_from_cbd(this_df)
+        self.violin_plot_price_per_region(this_df)
 
 
     def map_prices(self, this_df):
-        # import libraries
         import geopandas as gpd
         from shapely.geometry import Point, Polygon
         import matplotlib.pyplot as plt
 
-        df = this_df[this_df['Longtitude'].notna()]
-        df = this_df[this_df['Lattitude'].notna()]
-        df = this_df[this_df['Price'].notna()]
-
         # import street map
         street_map = gpd.read_file("./Maps/Metropolitan region_region.shp")
 
-        #zip x and y coordinates into single feature
-        geometry = [Point(xy) for xy in zip(df['Longtitude'], df['Lattitude'])]
+        # zip x and y coordinates into single feature
+        geometry = [Point(xy) for xy in zip(this_df['Longtitude'], this_df['Lattitude'])]
         # create GeoPandas dataframe
-        geo_df = gpd.GeoDataFrame(df, crs="EPSG:3112", geometry=geometry)
-        print(geo_df.head())
+        geo_df = gpd.GeoDataFrame(this_df, crs="EPSG:3112", geometry=geometry)
 
         # create figure and axes, assign to subplot
         fig, ax = plt.subplots(figsize=(15, 15))
@@ -36,34 +38,40 @@ class Visualize:
         # add legend
         # make datapoints transparent using alpha
         # assign size of points using markersize
-        geo_df.plot(column='Price_LG', ax = ax, alpha = 0.5, legend = True, markersize = 1, legend_kwds={'label': "Prices", 'orientation': "horizontal"})
+        geo_df.plot(column='Price_LG', ax = ax, alpha = 0.5, legend = True, markersize = 1, legend_kwds={'label': "Price"})
         # add title to graph
         plt.title('Melbourne Property Prices', fontsize = 15, fontweight ='bold')
 
         # set latitiude and longitude boundaries for map display
         plt.xlim(144.5, 145.50)
         plt.ylim(-38.2, -37.6)
-        # show map
-        plt.show()
 
+        plt.savefig("./Output/Melbourne_Property_Price_Map.png")
+        plt.clf()
+        plt.close()
 
 
     def scatter_plot_distance_from_cbd(self, this_df):
         import seaborn as sns
         import matplotlib.pyplot as plt
 
-        this_df['Property_Type'] = this_df.apply(lambda row: self.fetch_type_description(row), axis=1)
-
         sns.set(style='whitegrid')
-        sns.scatterplot(x="Distance",
-                            y="Price",
-                            hue="Property_Type",
-                            data=this_df)
-
+        sns.scatterplot(x="Distance", y="Price", hue="Property_Type", data=this_df)
         plt.savefig("./Output/Distance_From_CBD.png")
         plt.clf()
         plt.close()
 
+    def violin_plot_price_per_region(self, this_df):
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+
+        sns.set(style='whitegrid')
+
+        sns.boxplot(x="Region", y="Price_LG", data=this_df)
+
+        plt.savefig("./Output/Violin_plot_per_region.png")
+        plt.clf()
+        plt.close()
 
     def fetch_type_description(self, row):
         if row['Type'] == 'h':
@@ -75,15 +83,22 @@ class Visualize:
         return 'Other'
 
 
-    # def use_plotly(self):
-    #     import plotly.express as px
-    #
-    #     fig = px.scatter_geo(this_df, lat='Lattitude', lon='Longtitude', hover_name="Address")
-    #     fig.update_layout(title='World map', title_x=0.5)
-    #     fig.show()
-    #
-    #
-    #     fig = px.density_mapbox(this_df, lat='Lattitude', lon='Longtitude', z='Propertycount', radius=10,
-    #                             center=dict(lat=0, lon=180), zoom=0,
-    #                             mapbox_style="stamen-terrain")
-    #     fig.show()
+    def shorten_region_name(self, row):
+        if row['Regionname'] == 'Northern Metropolitan':
+            return 'N-Met'
+        if row['Regionname'] == 'Southern Metropolitan':
+            return 'S-Met'
+        if row['Regionname'] == 'Eastern Metropolitan':
+            return 'E-Met'
+        if row['Regionname'] == 'Western Metropolitan':
+            return 'W-Met'
+        if row['Regionname'] == 'South-Eastern Metropolitan':
+            return 'S.E.-Met'
+        if row['Regionname'] == 'Northern Victoria':
+            return 'N-Vic'
+        if row['Regionname'] == 'Eastern Victoria':
+            return 'E-Vic'
+        if row['Regionname'] == 'Western Victoria':
+            return 'W-Vic'
+        return 'Other'
+
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import insights
 
 choose_data = int(input("Press 1 to choose local data already downloaded OR press 2 to download latest data from Kaggle(requires an API Token)?"))
 
+# fetch data
 if choose_data == 1:
     print('Using local data now...please wait')
     fh = importing.Import(1)
@@ -15,20 +16,24 @@ else:
     print('Downloading from Kaggle...please wait')
     fh = importing.Import(0)
 
-df = fh.return_df()
+raw_df = fh.return_df()
 
+# perform EDA
+discovery.EDA(raw_df)
 
+# clean anomalies
+data_cleaning = cleaning.CleanData(raw_df)
+cleaned_df = data_cleaning.return_df()
 
-discovery.EDA(df)
+# encode
+pre_processing = preprocessing.Encode(cleaned_df)
+cleaned_df = pre_processing.return_df()
 
-data_cleaning = cleaning.CleanData(df)
-df = data_cleaning.return_df()
+# start modelling
+modelling.RunModels(cleaned_df)
 
-# pre_processing = preprocessing.Encode(df)
-# df = pre_processing.return_df()
-#
-# modelling.RunModels(df)
-#
-# tuning.TuneModel(df)
+# Tune best regression model
+tuning.TuneModel(cleaned_df)
 
-insights.Visualize(df)
+#   Perform final analysis on dataset
+insights.Visualize(raw_df)
